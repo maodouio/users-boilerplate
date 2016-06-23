a8b4aa28d8a36d9fc5806f7d3e4a36d756d2e4b3
limingth <limingth@gmail.com>
2016-06-23 10:28:40 +0800

    Add isSaaS to customer schema

--- client/customers/document_edit.js
+++
@@ -1,8 +0,0 @@
-Template.customerEdit.rendered = function() {
-};
-
-Template.customerEdit.helpers({
-});
-
-Template.customerEdit.events ({
-});
--- client/customers/document_show.js
+++
@@ -1,9 +0,0 @@
-Template.customerShow.rendered = function() {
-  document.title = "毛豆网项目 - " + this.data.title;
-};
-
-Template.customerShow.helpers({
-});
-
-Template.customerShow.events ({
-});
